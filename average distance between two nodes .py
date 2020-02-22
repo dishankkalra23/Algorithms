59212858610d495cc6542599aa237a50dbcfed75
Dishank Kalra <dishankalra23@gmail.com>
2020-02-22 12:52:55 +0530

    Update average distance between two nodes .py
    The average shortest path length is sum of all shortest paths between all vertices and divide number of all possible paths.
    Parameters:	
    G (NetworkX graph) –
    weight (None or string, optional (default = None)) – If None, every edge has weight/distance/cost 1. If a string, use this edge attribute as the edge weight. Any edge attribute not present defaults to 1.

--- average distance between two nodes .py
+++ average distance between two nodes .py
@@ -1,11 +1,24 @@
 import networkx as nx
-import matplotlib.pyplot as plt
+
+# using networkx
+# : for creation of graphs and manipulation of networks
 
 g = nx.Graph()
 
+# g: empty graph
+
 def add(g, i, j, w):
+    """
+    :param g: empty graph
+    :param i: node i
+    :param j: node j
+    :param w: weight b/w node i and j
+    :return: graph formed with added edges
+    """
     g.add_edge(i, j, weight=w)
 
+
+# Evaluating sum of the average degree of nodes with below provided data
 add(g, 1, 2, 2)
 add(g, 2, 3, 1)
 add(g, 1, 4, 1)
@@ -21,3 +34,4 @@ add(g, 4, 8, 1)
 add(g, 8, 7, 1)
 
 print(nx.average_shortest_path_length(g))
+# networkx.average_shortest_path_length(graph_name): Returns the average shortest path length
